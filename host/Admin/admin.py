bdf6f1b6306081cc2705703133ff55e5d078853a
Andres <cesarandrestorresbernal604@gmail.com>
2023-11-06 15:25:20 -0500

    Agregación cuenta de banco a distribuidor y plataforma a Dominio

--- host/Admin/admin.py
+++ host/Admin/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Distributor, Domain
 
 # Register your models here.
+admin.site.register(Distributor)
+admin.site.register(Domain)
--- host/Admin/models.py
+++ host/Admin/models.py
@@ -8,6 +8,7 @@ class Distributor(models.Model):
     mail= models.CharField(max_length=100,default='',null=True,blank=True)
     q_domains= models.IntegerField(max_length=3,default='',null=True,blank=True)
     category= models.CharField(max_length=20,default='',null=True,blank=True)
+    bank_account= models.CharField(max_length=100,default='',null=True,blank=True)
     def __str__(self):
         return self.name
     
@@ -15,5 +16,6 @@ class Domain(models.Model):
     name= models.CharField(max_length=200,default='',null=True,blank=True)
     id_Distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
     available= models.BooleanField(default=True)
+    plataform= models.CharField(max_length=200,default='',null=True,blank=True)
     def __str__(self):
         return self.name
