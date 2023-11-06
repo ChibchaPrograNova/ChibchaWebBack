17cc58b9475ccc71934651444c241ec7d0bc51c6
Andres <cesarandrestorresbernal604@gmail.com>
2023-11-06 12:58:37 -0500

    Realizacion Tablas distribuidor y dominio, falta administrador

--- host/Admin/models.py
+++ host/Admin/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Distributor(models.Model):
+    name= models.CharField(max_length=200,default='',null=True,blank=True)
+    nit= models.CharField(max_length=200,default='',null=True,blank=True)
+    address= models.CharField(max_length=100,default='',null=True,blank=True)
+    mail= models.CharField(max_length=100,default='',null=True,blank=True)
+    q_domains= models.IntegerField(max_length=3,default='',null=True,blank=True)
+    category= models.CharField(max_length=20,default='',null=True,blank=True)
+    def __str__(self):
+        return self.name
+    
+class Domain(models.Model):
+    name= models.CharField(max_length=200,default='',null=True,blank=True)
+    id_Distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
+    available= models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
