cf1941a6864af4d7fdac4f2165668adcf2161903
Andres <cesarandrestorresbernal604@gmail.com>
2023-11-14 11:33:24 -0500

    arreglos varios

--- host/host/urls.py
+++ host/host/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.views.decorators.csrf import csrf_exempt
+# urls.py
 
-# Aplica csrf_exempt a la vista del administrador
+from django.views.decorators.csrf import csrf_exempt
 admin.site.login = csrf_exempt(admin.site.login)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
