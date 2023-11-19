00f95da7bfa3576f45e16de0c673e39898569ac0
Lokitosi <andres.gamba2011@gmail.com>
2023-11-19 17:27:08 -0500

    PUT para actualizacion de usuario

--- host/Client/views.py
+++ host/Client/views.py
@@ -28,13 +28,30 @@ def user_view(request, *args, **kwargs):
         users = Client.objects.all()
         serializer = Client_Serializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
-    if request.method == 'POST':
+    elif request.method == 'POST':
         request_data=JSONParser().parse(request)
         serializer=Client_Serializer(data=request_data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data,status=status.HTTP_200_OK)
         return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        user_id = request.GET.get('id')
+        if user_id:
+            try:
+                user = Client.objects.get(id=user_id)
+                request_data = JSONParser().parse(request)
+                serializer = Client_Serializer(user, data=request_data)
+
+                if serializer.is_valid():
+                    serializer.save()
+                    return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+                return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            except Client.DoesNotExist:
+                return JsonResponse({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+        return JsonResponse({'error': 'Se requiere el parámetro "id" para la actualización'}, status=status.HTTP_400_BAD_REQUEST)
     
 def Plan_view(request, *args, **kwargs):
     if request.method == 'GET':
