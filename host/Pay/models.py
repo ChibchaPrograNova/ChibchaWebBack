9ccc0e03383fa4a9384befbbd70427eddbf5bbdd
Andres <cesarandrestorresbernal604@gmail.com>
2023-11-27 14:47:27 -0500

    quitar todos los cambios, falta comisiones

--- host/Pay/models.py
+++ host/Pay/models.py
@@ -26,8 +26,6 @@ class Pay(models.Model):
     amount= models.IntegerField(default=0)
     type= models.CharField(max_length=200,choices=PAY_TYPES,default='')
     date= models.DateTimeField(default=timezone.now)
-    id_Distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE, null=True, blank=True)
-
     def __str__(self):
         return str(self.id_Client) + " - " + str(self.id_Card)
     
--- host/Pay/urls.py
+++ host/Pay/urls.py
@@ -1,8 +1,8 @@
-from .views import Card_view,Pay_view,calculate_commissions
+from .views import Card_view,Pay_view
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns=[path('Card/', csrf_exempt(Card_view)),
              path('Pay/',csrf_exempt(Pay_view)),
-             path('Calculate_commissions/',csrf_exempt(calculate_commissions)),
+            
              ]
